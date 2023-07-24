4818149a7276a241b19333cd9ee5834f1a8cbd1b
peraltamartinezequiel <peraltamartinezequiel@hotmail.com>
2023-07-24 11:29:49 -0300

    Creación del buscador

--- ProyectoFinal/runserver.py
+++ ProyectoFinal/runserver.py
@@ -6,9 +6,4 @@ from os import environ
 from ProyectoFinal import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5000'))
-    except ValueError:
-        PORT = 5000
-    app.run(HOST, PORT)
+    app.run(debug=True)
